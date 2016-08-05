a8939863b0f82107e48b788509b18cc2fd84ed25
Jon-Mailes Gr <mail@jonni.it>
2016-08-05 04:39:44 +0200

    new/old license (GPL-2)

--- plugins/jonni.whitelist.php
+++ plugins/jonni.whitelist.php
@@ -1,5 +1,43 @@
 <?php
 
+/*
+ * Plugin: Whitelist
+ * ~~~~~~~~~~~~~~~~~~~
+ * Whitelist is a xAseco 2 plugin which manages a whitelist 
+ * and kicks/bans (or what ever you like to do) every connecting player not being 
+ * whitelisted on the server.
+ * It can be useful for tournaments where people may spread the password of the 
+ * server and players not allowed to play a match can join the server 
+ * and falsify the results.
+ * ----------------------------------------------------------------------------------
+ * Author:		Jonniboy (http://jonni.it/)
+ * Version:		0.1
+ * Date:		2016-08-05
+ * Copyright:	2016 by Jonniboy (http://jonni.it)
+ * System:		XAseco2/1.00+
+ * Game:		ManiaPlanet Trackmania2 (TM2)
+ * ----------------------------------------------------------------------------------
+ *
+ * LICENSE: This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License as published
+ * by the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA
+ *
+ * ----------------------------------------------------------------------------------
+ *
+ * Dependencies:
+ *  - none
+ */
+
 Aseco::registerEvent('onStartup', 'jwl_event_onStartup');
 Aseco::registerEvent('onPlayerConnect2', 'jwl_event_onPlayerConnect');
 
